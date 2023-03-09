e6e179934ffd467abc05aa2f675685ccced1cd7c
https://github.com/settings/admin <tafairry520@gmail.com>
2023-03-09 18:02:26 +0800

    20230309_訂單視窗編輯(內含手動置入tempOrder防bug)

---
+++ src/components/OrderModal.vue
@@ -0,0 +1,102 @@
+<template>
+  <div
+    class="modal fade"
+    id="exampleModal"
+    tabindex="-1"
+    aria-labelledby="modifyModalLabel"
+    aria-hidden="true"
+    ref="modal"
+  >
+    <div class="modal-dialog modal-xl" role="document">
+      <div class="modal-content border-0">
+        <div class="modal-header bg-dark text-white">
+          <h5 class="modal-title" id="modifyModalLabel">
+            <span>訂單內容</span>
+          </h5>
+          <button
+            type="button"
+            class="btn-close-white btn-close"
+            data-bs-dismiss="modal"
+            aria-label="Close"
+          ></button>
+        </div>
+        <div class="modal-body">
+          <div class="row">
+            <div class="col-sm-4">
+              <div class="mb-3">
+                <h2>用戶資料</h2>
+                <table>
+                  <tr>
+                    <td>姓名</td>
+                    <td>
+                      {{ tempOrder.user.name }}
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>Email</td>
+                    <td>
+                      {{ tempOrder.user.email }}
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>電話</td>
+                    <td>
+                      {{ tempOrder.user.tel }}
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>地址</td>
+                    <td>
+                      {{ tempOrder.user.address }}
+                    </td>
+                  </tr>
+                </table>
+              </div>
+            </div>
+            <div class="col">
+              <div class="mb-3">
+                <h2>訂單細節</h2>
+
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import modalMixin from '../mixins/modalMixin';
+
+export default {
+  props: {
+    order: {
+      type: Object,
+      default() { return {}; },
+    },
+  },
+  watch: {
+    order() {
+      this.tempOrder = this.order;
+    },
+  },
+  data() {
+    return {
+      modal: {},
+      tempOrder: {
+        user: {},
+        products: {},
+      },
+    };
+  },
+  methods: {
+  },
+  mixins: [
+    modalMixin,
+  ],
+};
+</script>
--- src/views/OrdersView.vue
+++ src/views/OrdersView.vue
@@ -11,37 +11,82 @@
       </tr>
     </thead>
     <tbody>
-      <tr>
-        <td class="text-right">2023/1/6</td>
-        <td>test123@123.com</td>
-        <td>測試 12300元</td>
-        <td>12300</td>
-        <td>false</td>
+      <tr v-for="item in orders" :key="item.id">
+        <td class="text-right">
+          {{ date(item.create_at) }}
+        </td>
+        <td>
+          {{ item.user.email }}
+        </td>
+        <td>
+          <ul>
+            <li v-for="(product, i) in item.products" :key="i">
+              {{ product.product.title }} 數量：{{ product.qty }}
+              {{ product.product.unit }}
+            </li>
+          </ul>
+        </td>
+        <td>{{ item.total }}</td>
+        <td>{{ item.is_paid }}</td>
         <td>
           <div class="btn-group">
-            <button class="btn btn-outline-primary btn-sm">修改</button>
-            <button class="btn btn-outline-danger btn-sm">刪除</button>
+            <button
+              class="btn btn-outline-primary btn-sm"
+              @click.prevent="openModal(false, item)"
+            >檢視</button>
+            <button
+              class="btn btn-outline-danger btn-sm"
+              @click.prevent="deleteOrder"
+            >刪除</button>
           </div>
         </td>
       </tr>
     </tbody>
   </table>
+  <OrderModal
+    ref="orderModal"
+    :order = "tempOrder"
+  ></OrderModal>
 </template>
 
 <script>
+import { date } from '../methods/filters';
+import OrderModal from '../components/OrderModal.vue';
+
 export default {
   data() {
     return {
       orders: [],
+      tempOrder: {},
       isLoading: false,
+      isNew: false,
     };
   },
+  components: {
+    OrderModal,
+  },
   methods: {
+    date,
     getOrders() {
       const api = `${process.env.VUE_APP_API}api/${process.env.VUE_APP_PATH}/admin/orders`;
       this.$http.get(api, this.user).then((res) => {
-        console.log(res);
+        this.orders = res.data.orders;
+        console.log(res.data.orders);
       });
+    },
+    openModal(isNew, item) {
+      if (isNew) {
+        this.tempOrder = {};
+      } else {
+        this.tempOrder = { ...item };
+      }
+
+      this.isNew = isNew;
+      const OrderComponent = this.$refs.orderModal;
+      OrderComponent.showModal();
+    },
+    deleteOrder() {
+      console.log('刪除按鍵測試');
     },
   },
   created() {
